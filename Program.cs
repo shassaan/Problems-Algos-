00f376e460759a5ae5088a16c1616ff88c2ea3fe
Syed Hassaan Ahmed <syedhassaan.103@gmail.com>
2021-01-30 13:30:39 +0500

    implemented matchstrings function

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace problemSolving
 {
@@ -7,17 +8,26 @@ namespace problemSolving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(matchingStrings(new string[]{""},new string[]{"1","2","3"}));
+            matchingStrings(new string[] { "" }, new string[] { "1", "2", "3" });
             Console.WriteLine("Hello World!");
         }
 
 
+        /// <summary>
+        ///     returns array containing frequencies of "qeuries" in "strings"
+        /// </summary>
+        /// <param name="strings">list of strings</param>
+        /// <param name="queries">list containing queries for which we want the frequencies</param>
+        /// <returns>array of integers containing frequencies</returns>
+
         static int[] matchingStrings(string[] strings, string[] queries)
         {
-            var queriesWithCounts = new Dictionary<string,int>();
-            queries.AddUniqueRange(queriesWithCounts.Add);
-            Console.WriteLine(string.Join(",",queriesWithCounts.Keys));
-            return null;
+            var counts = new List<int>();
+            foreach (var query in queries)
+            {
+                counts.Add(strings.Where(s => s == query).Count());
+            }
+            return counts.ToArray();
         }
     }
 }
