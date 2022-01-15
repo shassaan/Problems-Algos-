79a3a2f8160f661ed7ac7beae9b99addf98a2731
shassaan <syedhassaan.103@gmail.com>
2022-01-15 23:26:30 +0500

    [main] three sum problem

--- Program.cs
+++ Program.cs
@@ -128,6 +128,74 @@ namespace problemSolving
             return arr;
         }
 
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var triplets = new List<IList<int>>();
+            Array.Sort(nums);
+            for(int i = 0;i<nums.Length-2;i++)
+            {
+                if(i == 0 || (i > 0 && nums[i] != nums[i-1]))
+                {
+                    int low = i - 1;
+                    int high = nums.Length-1;
+                    int sum = 0 - nums[i];
+                    while(low < high)
+                    {
+                        if(nums[low] + nums[high] == sum)
+                        {
+                            triplets.Add(new List<int> {
+                                nums[i],nums[low],nums[high]
+                            });
+                            while(low < high && nums[low] == nums[low +1]) low ++;
+                            while(low < high && nums[high] == nums[high - 1]) high --;
+                            low ++;
+                            high --;
+                        }
+                        else if(nums[low] + nums[high] > sum)
+                        {
+                            high --;
+                        }
+                        else
+                        {
+                            low ++;
+                        }
+                        
+                    }
+                }
+            }
+            return triplets;
+        }
+
+        public IList<IList<int>> ThreeSumBrute(int[] nums)
+        {
+            IList<IList<int>> listOfList = new List<IList<int>>();
+            Dictionary<string,int> hashset = new Dictionary<string, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    for (int k = 2; k < nums.Length; k++)
+                    {
+                        if(nums[i] + nums[j] + nums[k] == 0 && i!=j  && i!=k && k!=j)
+                        {
+                            var triplet = new List<int> {
+                                nums[i],nums[j],nums[k]
+                            };
+                            triplet.Sort();
+                            var s = JsonSerializer.Serialize(triplet);
+                            if(!hashset.ContainsKey(s))
+                            {
+                                hashset.Add(s,i);
+                                listOfList.Add(triplet);
+                            }
+                                
+                        }
+                    }
+                }
+            }
+            return listOfList;
+        }
+
         public int[] TwoSumDictionary(int[] nums, int target)
         {
             int[] arr = new int[2];
